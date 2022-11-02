1318403e041a82e999ddbec700936bcc98b41984
Drew O'Keefe <okeefedrew@gmail.com>
2022-11-02 14:14:10 -0500

    tip first commit

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,31 @@
-describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+describe('Servers test (with setup and tear-down)', function () {
+	beforeEach(function () {
+		// initialization logic
+		serverNameInput.value = 'Alice';
+	});
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+	it('should add a new server to allServers on submitServerInfo()', function () {
+		submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+		expect(Object.keys(allServers).length).toEqual(1);
+		expect(allServers['server' + serverId].serverName).toEqual('Alice');
+	});
 
-  afterEach(function() {
-    // teardown logic
-  });
+	it('should update #servertable on updateServerTable()', function () {
+		submitServerInfo();
+		updateServerTable();
+
+		let curTdList = document.querySelectorAll('#serverTable tbody tr td');
+
+		expect(curTdList.length).toEqual(2);
+		expect(curTdList[0].innerText).toEqual('Alice');
+		expect(curTdList[1].innerText).toEqual('$0.00');
+	});
+
+	afterEach(function () {
+		// teardown logic
+		serverId = 0;
+		serverTbody.innerHTML = '';
+		allServers = {};
+	});
 });
